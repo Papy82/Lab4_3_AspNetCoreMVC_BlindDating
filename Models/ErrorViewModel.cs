efafac44f4f71c987e36cd6e43ab5f9731fb4cb0
Papy82 <62964929+Papy82@users.noreply.github.com>
2020-08-24 13:50:00 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab4_3_AspNetCoreMVC_BlindDating.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
